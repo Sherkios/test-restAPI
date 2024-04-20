a00705d6f2769a882333a5182a732e5249fe2c9a
Grishchenko Igor <Sherkiosp@gmail.com>
2024-04-20 13:12:16 +0500

    Небольшая правка в бд

--- server/db/migrations/20240419153940_create_users_table.js
+++ server/db/migrations/20240419153940_create_users_table.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('users', function (table) {
       table.increments('id').primary();
-      table.string('name', 255).notNullable().primary();
+      table.string('name', 255).notNullable();
       table.string('email').notNullable().unique();
       table.string('password', 255).notNullable();
     });
